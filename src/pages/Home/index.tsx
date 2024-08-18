7efc0f12afa43b2f7b7051f54974f0e464cc152c
Lucas Sales <sales_l@hotmail.com>
2024-08-18 19:42:32 -0300

    header done

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,22 @@
 import React from "react";
+import { Header } from "./styles";
 
 const Home = (): JSX.Element => {
-  return <div>Happy hacking!</div>;
+  return (
+    <Header className="border-b-4 border-amber-600">
+      <h1 className="font-mono font-bold">Memória, trauma e colonialismo</h1>
+      <h2 className="font-mono font-bold">Sobre a página</h2>
+      <p className="text-xl">
+        Este ensaio explora como tecnologias digitais podem ser usadas para
+        preservar memórias de traumas coloniais, inspirando-se nos trabalhos de
+        Graça Kilomba e Rita Segato. O projeto propõe a criação de um site
+        interativo como repositório de relatos pessoais e históricos, promovendo
+        a conscientização, o diálogo e a reconciliação. Além disso, destaca o
+        papel das redes sociais na ampliação da visibilidade dessas histórias e
+        na união das comunidades na luta pela justiça social.
+      </p>
+    </Header>
+  );
 };
 
 export default Home;
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,1 +1,26 @@
 import styled from "styled-components";
+import Banner from "../../assets/banner.png";
+
+export const Header = styled.header`
+  background-image: url(${Banner});
+  background-size: 100% 100%;
+  background-repeat: no-repeat;
+  display: flex;
+  flex-direction: column;
+  height: 30%;
+  h1 {
+    font-size: 200%;
+    margin-left: 10%;
+    margin-top: 2%;
+  }
+  h2 {
+    font-size: 200%;
+    margin: 10% 0 0 50%;
+    color: white;
+  }
+  p {
+    margin: 1% 0 10% 50%;
+    width: 35%;
+    color: white;
+  }
+`;
