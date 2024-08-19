359a3a8c9de7e0b4cd30882116a2c5d0838aecbe
Lucas Sales <sales_l@hotmail.com>
2024-08-19 15:33:19 -0300

    body structure

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,21 +1,67 @@
 import React from "react";
-import { Header } from "./styles";
+import {
+  Body,
+  Content,
+  ContentColum,
+  Header,
+  TextBox,
+  VerticalDivider,
+} from "./styles";
+import photo from "../../assets/banner.png";
 
 const Home = (): JSX.Element => {
   return (
-    <Header className="border-b-4 border-amber-600">
-      <h1 className="font-mono font-bold">Memória, trauma e colonialismo</h1>
-      <h2 className="font-mono font-bold">Sobre a página</h2>
-      <p className="text-xl">
-        Este ensaio explora como tecnologias digitais podem ser usadas para
-        preservar memórias de traumas coloniais, inspirando-se nos trabalhos de
-        Graça Kilomba e Rita Segato. O projeto propõe a criação de um site
-        interativo como repositório de relatos pessoais e históricos, promovendo
-        a conscientização, o diálogo e a reconciliação. Além disso, destaca o
-        papel das redes sociais na ampliação da visibilidade dessas histórias e
-        na união das comunidades na luta pela justiça social.
-      </p>
-    </Header>
+    <>
+      <Header className="border-b-4 border-amber-600">
+        <h1 className="font-mono font-bold">Memória, trauma e colonialismo</h1>
+        <h2 className="font-mono font-bold">Sobre a página</h2>
+        <p className="text-xl">
+          Este ensaio explora como tecnologias digitais podem ser usadas para
+          preservar memórias de traumas coloniais, inspirando-se nos trabalhos
+          de Graça Kilomba e Rita Segato. O projeto propõe a criação de um site
+          interativo como repositório de relatos pessoais e históricos,
+          promovendo a conscientização, o diálogo e a reconciliação. Além disso,
+          destaca o papel das redes sociais na ampliação da visibilidade dessas
+          histórias e na união das comunidades na luta pela justiça social.
+        </p>
+      </Header>
+      <Body>
+        <h1>Ao que se refere memória, trauma e colonialismo?</h1>
+        <Content>
+          <ContentColum>
+            <img src={photo} alt="" />
+            <TextBox>
+              <h3>Algum titulo aqui</h3>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed eos
+                maiores dolorem expedita fuga doloribus odio asperiores tempora
+                exercitationem autem nesciunt non quasi ad reprehenderit vero,
+                dolores officiis veniam rem! Aut totam sunt aspernatur magnam
+                veniam dolor odio odit. A accusantium repudiandae quam mollitia
+                ex earum natus illum consequatur qui, molestiae officiis
+                deleniti nisi fugiat dicta consectetur voluptas quos nihil!
+              </p>
+            </TextBox>
+          </ContentColum>
+          <VerticalDivider />
+          <ContentColum>
+            <TextBox>
+              <h3>Algum titulo aqui</h3>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed eos
+                maiores dolorem expedita fuga doloribus odio asperiores tempora
+                exercitationem autem nesciunt non quasi ad reprehenderit vero,
+                dolores officiis veniam rem! Aut totam sunt aspernatur magnam
+                veniam dolor odio odit. A accusantium repudiandae quam mollitia
+                ex earum natus illum consequatur qui, molestiae officiis
+                deleniti nisi fugiat dicta consectetur voluptas quos nihil!
+              </p>
+            </TextBox>
+            <img src={photo} alt="" />
+          </ContentColum>
+        </Content>
+      </Body>
+    </>
   );
 };
 
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -24,3 +24,51 @@ export const Header = styled.header`
     color: white;
   }
 `;
+
+export const Body = styled.div`
+  height: fit-content;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: #f1f1f1;
+  h1 {
+    margin: 3% 0;
+  }
+`;
+export const Content = styled.div`
+  display: grid;
+  grid-template-columns: 48% 0.3% 48%;
+  gap: 0.5%;
+  padding: 0 15%;
+`;
+
+export const ContentColum = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  img,
+  div {
+    margin: 3% 0;
+    height: 40%;
+  }
+  h3 {
+    text-align: right;
+  }
+  &:nth-child(3) {
+    h3 {
+      text-align: left;
+    }
+  }
+`;
+
+export const TextBox = styled.div`
+  width: fill;
+  height: 100%;
+  overflow: scroll;
+`;
+
+export const VerticalDivider = styled.div`
+  height: 100%;
+  width: 100%;
+  background-color: orange;
+`;
